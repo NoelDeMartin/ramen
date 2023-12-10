ba37223c6d1e365defa27a671a6501be00828d6b
Noel De Martin <noel@noeldemartin.com>
2023-12-10 11:36:09 +0100

    Remove superfluous tests

---
+++ src/models/Recipe.test.ts
@@ -0,0 +1,21 @@
+import { bootSolidModels } from 'soukai-solid';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { InMemoryEngine, bootModels, setEngine } from 'soukai';
+
+import Recipe from './Recipe';
+
+describe('Recipe model', () => {
+
+    beforeEach(async () => {
+        bootSolidModels();
+        bootModels({ Recipe });
+        setEngine(new InMemoryEngine());
+    });
+
+    it('identifies Ramen', () => {
+        expect(new Recipe({ name: 'Ramen' }).isRamen()).toBe(true);
+        expect(new Recipe({ name: 'Jun\'s Ramen' }).isRamen()).toBe(true);
+        expect(new Recipe({ name: 'Pizza' }).isRamen()).toBe(false);
+    });
+
+});
